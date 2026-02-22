1357484b55da9d2a368f1a566bc152d39646dab9
clinicasonlinepro-ai <clinicasonline.pro@gmail.com>
2026-02-21 21:56:44 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Hamburger toggle
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
